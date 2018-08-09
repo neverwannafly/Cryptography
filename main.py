e56fe69f30e7ca5151d15b9557e6fde077809a97
Shubham Anand <shubham_chess@live.com>
2018-08-10 00:21:56 +0530

    change plain text

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from PlayfairCipher import PlayfairCipher
 
 if __name__=='__main__':
     cipher = PlayfairCipher('MYSECRETCODY')
-    cipher.set_message("heelllllooooo")
+    cipher.set_message("helelele my name is ashubham")
     encrypted_message = cipher.encrypt()
     print(encrypted_message)
     cipher.set_encrypted_message(encrypted_message)
