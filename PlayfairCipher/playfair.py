9b2e29f55674438ff6694beb3b9e53a075a5a150
Shubham Anand <shubham_chess@live.com>
2018-08-10 00:10:34 +0530

    add decrypt method

--- PlayfairCipher/playfair.py
+++ PlayfairCipher/playfair.py
@@ -3,7 +3,7 @@
 import random
 import string
 
-from exceptions import InvalidKeyFormat, InvalidMessageFormat
+from exceptions import InvalidKeyFormat, InvalidMessageFormat, InvalidEncryptedMessage, NoMessageAttached
 
 class PlayfairCipher:
     
@@ -12,10 +12,16 @@ class PlayfairCipher:
         self.key = self._attenuate_key(key)
         self.keymap = self._generate_keymap()
         self.message = None
+        self.encrypted_message = None
+        print(self.keymap)
 
     def set_message(self, message):
         self._check_message_validity(message)
         self.message = message
+
+    def set_encrypted_message(self, enc_message):
+        self._check_encrypted_message_validity(enc_message)
+        self.encrypted_message = enc_message
 
     def _check_key_validity(self, key):
         if len(key)>25 or not key.isupper():
@@ -24,6 +30,10 @@ class PlayfairCipher:
     def _check_message_validity(self, message):
         if not message.islower():
             raise InvalidMessageFormat("Message must be in lowercase english letters!")
+
+    def _check_encrypted_message_validity(self, enc_message):
+        if not enc_message.isupper():
+            raise InvalidEncryptedMessage("Encrypted Message must be in upercase english letters!")
 
     def _generate_key(self, key):
         initial_key = list(key)
@@ -59,29 +69,44 @@ class PlayfairCipher:
         index = x-1 if x-1>=0 else 0
         return self.keymap[index][y]
         
-    def _is_same_row(self, l1, l2):
+    def _is_same_row(self, l1, l2, do_decrypt=False):
         search_l1 = self._search_letter(l1)
         search_l2 = self._search_letter(l2)
         if search_l1[0]==search_l2[0]:
+            if not do_decrypt:
+                return (True, "{0}{1}".format(
+                    self._get_right(search_l1[0], search_l1[1]), 
+                    self._get_right(search_l2[0], search_l2[1])
+                ))
             return (True, "{0}{1}".format(
-                self._get_right(search_l1[0], search_l1[1]), 
-                self._get_right(search_l2[0], search_l2[1])
+                self._get_left(search_l1[0], search_l1[1]), 
+                self._get_left(search_l2[0], search_l2[1])
             ))
         return (False, None)
 
-    def _is_same_column(self, l1, l2):
+    def _is_same_column(self, l1, l2, do_decrypt=False):
         search_l1 = self._search_letter(l1)
         search_l2 = self._search_letter(l2)
         if search_l1[1]==search_l2[1]:
+            if not do_decrypt:
+                return (True, "{0}{1}".format(
+                    self._get_below(search_l1[0], search_l1[1]), 
+                    self._get_below(search_l2[0], search_l2[1])
+                ))
             return (True, "{0}{1}".format(
-                self._get_below(search_l1[0], search_l1[1]), 
-                self._get_below(search_l2[0], search_l2[1])
-            ))
+                self._get_top(search_l1[0], search_l1[1]), 
+                self._get_top(search_l2[0], search_l2[1])
+            )) 
         return (False, None)
 
-    def _matrix_match(self, l1, l2):
+    def _matrix_match(self, l1, l2, do_decrypt=False):
         search_l1 = self._search_letter(l1)
         search_l2 = self._search_letter(l2)
+        if not do_decrypt:
+            return "{0}{1}".format(
+                self.keymap[search_l1[0]][search_l2[1]],
+                self.keymap[search_l2[0]][search_l1[1]]
+            )
         return "{0}{1}".format(
             self.keymap[search_l1[0]][search_l2[1]],
             self.keymap[search_l2[0]][search_l1[1]]
@@ -137,9 +162,10 @@ class PlayfairCipher:
             plain_text.append(message[len(message)-1] + random.choice(string.ascii_lowercase))
         return plain_text
 
-    def _process_plain_text(self, message):
+    def _process_plain_text(self, message, do_decrypt=False):
         message = self._attenuate_message(message)
-        message = self._remove_repetetion(message)
+        if not do_decrypt:
+            message = self._remove_repetetion(message)
         plain_text = self._break_message(message)
         return plain_text
 
@@ -150,6 +176,7 @@ class PlayfairCipher:
         encrypted_message = ""
 
         plain_text = self._process_plain_text(self.message)
+        print(plain_text)
 
         for text in plain_text:
             same_row_try = self._is_same_row(text[:1].upper(), text[1:].upper())
@@ -160,14 +187,31 @@ class PlayfairCipher:
                 encrypted_message += same_column_try[1]
             else:
                 encrypted_message += self._matrix_match(text[:1].upper(), text[1:].upper())
-        
         return encrypted_message
 
 
     def decrypt(self):
-        pass
+        if self.encrypted_message is None:
+            raise NoMessageAttached("Please attach a message by calling the set_encrypted_message method!")
+
+        decrypted_message = ""
+
+        plain_text = self._process_plain_text(self.encrypted_message, do_decrypt=True)
+        for text in plain_text:
+            same_row_try = self._is_same_row(text[:1], text[1:], do_decrypt=True)
+            same_column_try = self._is_same_column(text[:1], text[1:], do_decrypt=True)
+            if same_row_try[0]:
+                decrypted_message += same_row_try[1].lower()
+            elif same_column_try[0]:
+                decrypted_message += same_column_try[1].lower()
+            else:
+                decrypted_message += self._matrix_match(text[:1], text[1:], do_decrypt=True).lower()
+        return decrypted_message
 
 a = PlayfairCipher("ANOTHER")
-a.set_message("we live in a world full of beauty")
+a.set_message("eno")
 b = a.encrypt()
+a.set_encrypted_message(b)
+c = a.decrypt()
 print(b)
+print(c)
